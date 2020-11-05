e38c935e9a260656778d64bf9af2fc1d126cde83
Ivan Vieira <ivanjr.ce@gmail.com>
2020-11-04 23:30:49 -0300

    Refactor code & basic config db with knex

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const server = express();
-
-server.get('/', (request, response) =>
-  response.json({ message: 'Teste Servidor'}),
-);
-
-server.listen(3333);
