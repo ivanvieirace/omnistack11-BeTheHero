49f78b67d6910ec39e9c9bd57297dfa7f9562344
Ivan Vieira <ivanjr.ce@gmail.com>
2020-11-12 13:58:51 -0300

    Upate Header Component & Format Code

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Header from './Header';
 
 function App() {
-  return <div className="App"></div>;
+  return <Header title="Be The Hero" />;
 }
 
 export default App;
