0dca7334b54d321e6d4efa4dd0e0eacd4271f08b
Ivan Vieira <ivanjr.ce@gmail.com>
2020-11-28 11:28:11 -0300

    Add Prettier config, pages, splash screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Be The Hero</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Be The Hero</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  title: {
-    fontSize: 40,
-    fontWeight: "bold",
-  },
-});
