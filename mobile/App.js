ec996ccf7260132892f92f801b59943a23b0e219
Ivan Vieira <ivanjr.ce@gmail.com>
2020-11-27 23:42:34 -0300

    Project Mobile - First Mobile Update

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Be The Hero</Text>
+      <Text style={styles.title}>Be The Hero</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
   },
 });
