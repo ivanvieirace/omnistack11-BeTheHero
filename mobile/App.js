9dca473acec5e3fe74a81c5e7ea7ab5df3f3865f
Ivan Vieira <ivanjr.ce@gmail.com>
2020-11-30 23:55:18 -0300

    Create routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Be The Hero</Text>
-    </View>
-  );
+  return <Routes />;
 }
